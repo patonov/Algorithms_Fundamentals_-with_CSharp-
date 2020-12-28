d61287e9e6bd9b98a20b62963e041a937114c627
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-28 17:37:08 +0200

    Add files via upload

---
+++ Recursive Array Sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int result = CalcSum(num, 0);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static int CalcSum(int[] numArr, int index)
+        {
+            if (index >= numArr.Length)
+            {
+                return 0;
+            }
+            return numArr[index] + CalcSum(numArr, index + 1);
+        }
+    }
+    }
+
