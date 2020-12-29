f45e67a2811475e9452d5b9d4cab5cf615c6f1a3
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-29 22:07:30 +0200

    Add files via upload

---
+++ Recursive Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int result = CalcFact(num);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static int CalcFact(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num * CalcFact(num - 1);
+        }
+    }
+    }
