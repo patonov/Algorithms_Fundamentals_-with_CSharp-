fc00e4f20a167ca055657ad3773d83f7f7721b76
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-29 22:08:55 +0200

    Add files via upload

---
+++ Recursive Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFabonacci(n));
+
+
+        }
+        private static int GetFabonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            return GetFabonacci(n - 1) + GetFabonacci(n - 2);
+        }
+    }
+    }
+
