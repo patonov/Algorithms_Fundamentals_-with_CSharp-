ae2f6a9705e50a6786f8cb7dfd8de5109c2e9049
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-29 22:06:10 +0200

    Add files via upload

---
+++ Recursive Drawing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            DrawGraph(num);
+
+        }
+
+        public static void DrawGraph(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', n));
+            DrawGraph(n - 1);
+            Console.WriteLine(new string('#', n));
+        }
+    }
+    }
+
