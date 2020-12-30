df1333cd5001c7c52de0024ca366f6f9193b1fdd
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-30 18:20:07 +0200

    Add files via upload

---
+++ Permutations with Repetition.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations_with_Repetition
+{
+    class Program
+    {
+        private static string[] elements;
+
+        public static void Main()
+        {
+
+            elements = Console.ReadLine().Split();
+
+            Permute(0);
+
+        }
+        private static void Permute(int cellIndex)
+        {
+            if (cellIndex >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            Permute(cellIndex + 1);
+
+            var swapped = new HashSet<string> { elements[cellIndex] };
+
+            for (int i = cellIndex + 1; i < elements.Length; i++)
+            {
+                if (!swapped.Contains(elements[i]))
+                {
+                    Swap(cellIndex, i);
+                    Permute(cellIndex + 1);
+                    Swap(cellIndex, i);
+                    swapped.Add(elements[i]);
+
+                }
+            }
+        }
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
+
