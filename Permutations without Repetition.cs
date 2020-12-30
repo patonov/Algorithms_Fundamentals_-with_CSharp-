030bd83932f2c632147f9acea82e1e4f084c405e
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-30 18:19:41 +0200

    Add files via upload

---
+++ Permutations without Repetition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations_without_Repetition
+{
+    class Program
+    {
+        private static string[] elements;
+        private static string[] permutations;
+        private static bool[] used;
+
+        public static void Main()
+        {
+
+            elements = Console.ReadLine().Split();
+            permutations = new string[elements.Length];
+            used = new bool[elements.Length];
+
+            Permute(0);
+        }
+        private static void Permute(int cellIndex)
+        {
+            if (cellIndex >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+            for (int elementInx = 0; elementInx < elements.Length; elementInx++)
+            {
+                if (!used[elementInx])
+                {
+                    used[elementInx] = true;
+                    permutations[cellIndex] = elements[elementInx];
+                    Permute(cellIndex + 1);
+                    used[elementInx] = false;
+                }
+            }
+        }
+    }
+}
+        
+
