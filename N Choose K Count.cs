8a8e90c6045fd8dc089b172f952efbaa1fc478e4
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2020-12-31 18:27:50 +0200

    Add files via upload

---
+++ N Choose K Count.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Choose_K_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinominal(n, k));
+
+        }
+        public static int GetBinominal(int row, int column)
+        {
+            if (row <= 0 || row == 1 || column == 0 || column == row)
+            {
+                return 1;
+            }
+            return GetBinominal(row - 1, column - 1) + GetBinominal(row - 1, column);
+
+        }
+    }
+    }
+
