d2154721426165a5cc2bb412702f3fe25771d456
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-01 16:43:12 +0200

    Add files via upload

---
+++ Reverse Array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine().Split();
+
+            for (int left = 0; left < n.Length / 2; left++)
+            {
+                int right = n.Length - 1 - left;
+                Swap(n, left, right);
+            }
+            Console.WriteLine(string.Join(" ", n));
+
+
+        }
+        public static void Swap(string[] n, int left, int right)
+        {
+            var temp = n[left];
+            n[left] = n[right];
+            n[right] = temp;
+
+        }
+    }
+  }
+
