ef936667e368cd61611b62cacbf840617d291fc9
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-03 17:08:56 +0200

    Add files via upload

---
+++ Merge Sort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var sorted = MergeSort(numbers);
+
+            Console.WriteLine(string.Join(" ", sorted));
+
+        }
+        private static int[] MergeSort(int[] numbers)
+        {
+            if (numbers.Length <= 1)
+            {
+                return numbers;
+            }
+            var left = numbers.Take(numbers.Length / 2).ToArray();
+            var right = numbers.Skip(numbers.Length / 2).ToArray();
+
+            return Merge(MergeSort(left), MergeSort(right));
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            var merged = new int[left.Length + right.Length];
+
+            var mergedIndex = 0;
+            var leftIndex = 0;
+            var rightIndex = 0;
+
+            while (leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    merged[mergedIndex] = left[leftIndex];
+                    leftIndex += 1;
+                }
+                else
+                {
+                    merged[mergedIndex] = right[rightIndex];
+                    rightIndex += 1;
+                }
+                mergedIndex += 1;
+            }
+
+            while (leftIndex < left.Length)
+            {
+                merged[mergedIndex] = left[leftIndex];
+                leftIndex += 1;
+                mergedIndex += 1;
+            }
+
+            while (rightIndex < right.Length)
+            {
+                merged[mergedIndex] = right[rightIndex];
+                rightIndex += 1;
+                mergedIndex += 1;
+            }
+            return merged;
+        }
+    }
+  }
