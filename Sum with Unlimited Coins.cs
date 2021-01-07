49342acd5348c3094ee1deb61088efcc8335a97a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-01-07 17:05:18 +0200

    Add files via upload

---
+++ Sum with Unlimited Coins.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_with_Unlimited_Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            var count = GetCount(coins, target);
+
+            Console.WriteLine(count);
+
+        }
+        private static int GetCount(int[] coins, int target)
+        {
+            var sums = new int[target + 1];
+            sums[0] = 1;
+
+            foreach (var coin in coins)
+            {
+                for (int sum = coin; sum < sums.Length; sum++)
+                {
+                    sums[sum] += sums[sum - coin];
+                }
+            }
+            return sums[target];
+        }
+    }
+}
